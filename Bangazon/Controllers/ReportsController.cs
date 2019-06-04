bd68107699b2c024017f9c467f3ed7e4b7c70102
Trevan Young <trevan.young@gmail.com>
2019-06-04 11:55:10 -0500

    In progress for ticket # 11

--- Bangazon/Controllers/ReportsController.cs
+++ Bangazon/Controllers/ReportsController.cs
@@ -25,6 +25,25 @@ namespace Bangazon.Controllers
 
         private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
 
-        //gets go here
+        // GET: Orders
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Order.Include(o => o.PaymentType).Include(o => o.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        public async Task<IActionResult> GetOrdersByTopFiveProductTypesIncompleted(int id)
+        {
+            var orders = _context.Order
+                .Include(o => o.OrderProducts)
+                .ThenInclude(p => p.Product)
+                .ThenInclude(p => p.ProductType);
+                //.Where(p => p.ProductTypeId == id);
+            if (orders == null)
+            {
+                return NotFound();
+            }
+            return View(await orders.ToListAsync());
+        }
     }
 }
